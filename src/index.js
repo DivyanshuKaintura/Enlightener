952f3b8e40545cacfa2350180d0396a5d633fd7b
DivyanshuKaintura <110197741+DivyanshuKaintura@users.noreply.github.com>
2024-08-31 12:27:02 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Deep from './components/Deep.jsx';
 import Header from './components/Header.jsx';
 import Chat from './components/Chat.jsx';
 import Footer from './components/Footer.jsx'
+import Details from './components/Details.jsx';
+import Awareness from './components/Awareness.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +14,8 @@ root.render(
     <Header />
     <Deep />
     <Chat />
+    <Details />
+    <Awareness />
     <Footer />
   </React.StrictMode>
 );
