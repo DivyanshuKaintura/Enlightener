1e22973f15069b8c6771ff7d8cb5576c44593d97
DivyanshuKaintura <110197741+DivyanshuKaintura@users.noreply.github.com>
2024-08-31 08:21:50 +0530

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,15 @@
 import React from "react";
-// import {
-//     BrowserRouter as Router,
-//     Routes,
-//     Route,
-//     Navigate,
-// } from 'react-router-dom';
 import './header.css'
-// import Home from "./Home";
 
 function Header() {
+
     return (
         <div className="header">
-            {/* <a href="#default" class="logo">ENLIGHTENER</a> */}
             <h1 className="name">ENLIGHTENER</h1>
             <div className="header-right">
-                <a href="#home">Home</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-                {/* <Router>
-                    <Routes>
-                        <Route exact path="/" element={<Home />} />
-
-                        <Route
-                            path="*"
-                            element={<Navigate to="/" />}
-                        />
-                    </Routes>
-                </Router> */}
+                <a href="#home" onClick={()=> window.scrollTo(0, 0)}>Home</a> 
+                <a href="#contact" onClick={()=> window.scrollTo(0, 1522)}>Contact</a>
+                <a href="#about" onClick={()=> window.scrollTo(0, 1522)}>About</a>
             </div>
         </div>
     )
